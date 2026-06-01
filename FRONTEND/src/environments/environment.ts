28bf36a8d682641c63623caf854eeec74b40c772
LFifMan <finociety.valentin@gmail.com>
2026-06-01 17:45:09 +0200

    fix migrations history & plug local db, npm start & login OK

--- FRONTEND/src/environments/environment.ts
+++ FRONTEND/src/environments/environment.ts
@@ -1,5 +1,10 @@
+//export const environment = {
+//  production: true,
+//  supabaseUrl: 'https://ttrgqgxmkeunwpraqhsd.supabase.co',
+//  supabaseAnonKey: 'sb_publishable_WJOVpSFssYkv4OWFTt9d_Q_fBU0ujkO',
+//};
 export const environment = {
-  production: true,
-  supabaseUrl: 'https://ttrgqgxmkeunwpraqhsd.supabase.co',
-  supabaseAnonKey: 'sb_publishable_WJOVpSFssYkv4OWFTt9d_Q_fBU0ujkO',
+  production: false,
+  supabaseUrl: 'http://127.0.0.1:54321',
+  supabaseAnonKey: 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH',
 };
